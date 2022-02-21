027d40950366b801956e96f31607f73efe61010e
Jensen <jensenhuang2@gmail.com>
2022-02-21 16:56:05 +0800

    First commit

---
+++ configs/r3det/datasets/dotav1_rotational_detection.py
@@ -0,0 +1,76 @@
+# dataset settings
+# dataset_type = 'DOTADatasetV1'
+dataset_type = 'PIGLETDataset'
+# dataset root path:
+data_root = '/home/nas/Research_Group/Personal/Kim/piglet/r3det_test/data/'
+trainsplit_ann_folder = '/home/nas/Research_Group/Personal/Kim/piglet/r3det_test/data/trainsplit/labelTxt'
+trainsplit_img_folder = '/home/nas/Research_Group/Personal/Kim/piglet/r3det_test/data/trainsplit/images'
+valsplit_ann_folder = '/home/nas/Research_Group/Personal/Kim/piglet/r3det_test/data/valsplit/labelTxt'
+valsplit_img_folder = '/home/nas/Research_Group/Personal/Kim/piglet/r3det_test/data/valsplit/images'
+val_ann_folder = '/home/nas/Research_Group/Personal/Kim/piglet/r3det_test/data/val/labelTxt'
+val_img_folder = '/home/nas/Research_Group/Personal/Kim/piglet/r3det_test/data/val/images'
+test_img_folder = '/home/nas/Research_Group/Personal/Kim/piglet/r3det_test/data/test/images'
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RResize', img_scale=(600, 600)),
+    dict(type='RRandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    # dict(type='Pad', size=(800, 800)),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_bboxes_ignore']),
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='CroppedTilesFlipAug',
+        tile_scale=(600, 600),
+        tile_shape=(600, 600),
+        tile_overlap=(0, 0),
+        flip=False,
+        transforms=[
+            dict(type='RResize', img_scale=(600, 600)),
+            # dict(type='RRandomFlip', flip_ratio=0.5),
+            dict(type='Normalize', **img_norm_cfg),
+            # dict(type='Pad', size=(800, 800)),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+data = dict(
+    samples_per_gpu=6,
+    workers_per_gpu=3,
+    train=[
+        dict(
+            type=dataset_type,
+            data_root=data_root,
+            ann_file=trainsplit_ann_folder,
+            img_prefix=trainsplit_img_folder,
+            pipeline=train_pipeline),
+        dict(
+            type=dataset_type,
+            data_root=data_root,
+            ann_file=valsplit_ann_folder,
+            img_prefix=valsplit_img_folder,
+            pipeline=train_pipeline),
+    ],
+    val=dict(
+            type=dataset_type,
+            data_root=data_root,
+            ann_file=val_ann_folder,
+            difficulty_thresh=1,
+            img_prefix=val_img_folder,
+            pipeline=test_pipeline),
+    test=dict(
+        type=dataset_type,
+        data_root=data_root,
+        ann_file=test_img_folder,
+        difficulty_thresh=1,
+        img_prefix=test_img_folder,
+        pipeline=test_pipeline))
---
+++ rtools/dota_result_visualize.py
@@ -0,0 +1,71 @@
+import torch
+from mmcv import Config
+from mmcv.parallel import MMDataParallel
+from mmcv.runner import load_checkpoint
+
+from mmdet.apis import single_gpu_mergetiles_visualize
+from mmdet.core import wrap_fp16_model
+from mmdet.datasets import build_dataloader, build_dataset
+from mmdet.models import build_detector
+
+import argparse
+from torchinfo import summary
+# from torchsummary import summary
+# from pytorch_modelsize import SizeEstimator
+
+out_dir = "/home/nas/Research_Group/Personal/Kim/piglet/r3det_test/result_image/"
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Visualize result with tile-cropped images')
+    parser.add_argument('config', help='test config file path')
+    parser.add_argument('checkpoint', help='checkpoint file')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    cfg = Config.fromfile(args.config)
+    # set cudnn_benchmark
+    if cfg.get('cudnn_benchmark', False):
+        torch.backends.cudnn.benchmark = True
+    cfg.model.pretrained = None
+    cfg.data.test.test_mode = True
+
+    # build the dataloader
+    dataset = build_dataset(cfg.data.test)
+    data_loader = build_dataloader(
+        dataset,
+        samples_per_gpu=1,
+        workers_per_gpu=cfg.data.workers_per_gpu,
+        dist=False,
+        shuffle=False)
+
+    # build the model and load checkpoint
+    model = build_detector(cfg.model, train_cfg=None, test_cfg=cfg.test_cfg)
+    fp16_cfg = cfg.get('fp16', None)
+    if fp16_cfg is not None:
+        wrap_fp16_model(model)
+    checkpoint = load_checkpoint(model, args.checkpoint, map_location='cpu')
+
+    # old versions did not save class info in checkpoints, this walkaround is
+    # for backward compatibility
+
+    if 'CLASSES' in checkpoint['meta']:
+        model.CLASSES = checkpoint['meta']['CLASSES']
+    else:
+        model.CLASSES = dataset.CLASSES
+
+
+    model = MMDataParallel(model, device_ids=[0])
+
+    # summary(model, (3, 600, 600))
+    # se = SizeEstimator(model, input_size=(3,1,600,600))
+    # print(se.estimate_size())
+
+    single_gpu_mergetiles_visualize(model, data_loader, out_dir, 0.5)
+
+
+
+if __name__ == "__main__":
+    main()
